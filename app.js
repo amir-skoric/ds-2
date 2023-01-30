61526d1ab9c8919d85a43d6e69d3e50fb2d3bf71
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-01-30 17:18:00 +0100

    added randomfunction

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ hashBtn.addEventListener("click", () => {
     var result = CryptoJS[value](hashText.value)
     hashedText.innerHTML = result
 })
+
+
+//noget jeg fandt online, kan ikke helt finde ud af hvordan man laver den der one-time-pad, men den her funktion udskriver en str med random characters fra characters variablen :D
+function randomStr(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
